93acbc50dfe7fce16a9adfe5af39d11bce10d346
buianhdung58 <anhdungbui2000@gmail.com>
2021-05-08 16:56:44 +0700

    upload

--- BTL/Controllers/KhachhangsController.cs
+++ BTL/Controllers/KhachhangsController.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Data;
 using System.Data.Entity;
+using System.Data.OleDb;
+using System.Data.SqlClient;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Web;
@@ -113,6 +117,80 @@ namespace BTL.Controllers
             db.Khachhangs.Remove(khachhang);
             db.SaveChanges();
             return RedirectToAction("Index");
+        }
+        public ActionResult UploadFile(HttpPostedFileBase file)
+        {
+            //dat ten cho file
+            string _FileName = "File Name";
+            //duong dan luu file
+            string _path = Path.Combine(Server.MapPath("~/Uploads/Excels"), _FileName);
+            //luu file len server
+            file.SaveAs(_path);
+
+            //doc du lieu tu file Excel
+            DataTable dt = ReadDataFromExcelFile(_path);
+            CopyDataByBulk(dt);
+            //DataTable dt = excel.ReadDataFromExcelFile(_path);
+            return View("Index");
+        }
+        public DataTable ReadDataFromExcelFile(string filepath)
+        {
+            string connectionString = "";
+            string fileExtention = filepath.Substring(filepath.Length - 4).ToLower();
+            if (fileExtention.IndexOf("xlsx") == 0)
+            {
+                connectionString = "Provider = Microsoft.ACE.OLEDB.12.0; Data Source =" + filepath + ";Extended Properties=\"Excel 12.0 Xml;HDR=NO\"";
+            }
+            else if (fileExtention.IndexOf("xls") == 0)
+            {
+                connectionString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + filepath + ";Extended Properties=Excel 8.0";
+            }
+
+            // Tạo đối tượng kết nối
+            OleDbConnection oledbConn = new OleDbConnection(connectionString);
+            DataTable data = null;
+            try
+            {
+                // Mở kết nối
+                oledbConn.Open();
+
+                // Tạo đối tượng OleDBCommand và query data từ sheet có tên "Sheet1"
+                OleDbCommand cmd = new OleDbCommand("SELECT * FROM [Sheet1$]", oledbConn);
+
+                // Tạo đối tượng OleDbDataAdapter để thực thi việc query lấy dữ liệu từ tập tin excel
+                OleDbDataAdapter oleda = new OleDbDataAdapter();
+
+                oleda.SelectCommand = cmd;
+
+                // Tạo đối tượng DataSet để hứng dữ liệu từ tập tin excel
+                DataSet ds = new DataSet();
+
+                // Đổ đữ liệu từ tập excel vào DataSet
+                oleda.Fill(ds);
+
+                data = ds.Tables[0];
+            }
+            catch
+            {
+            }
+            finally
+            {
+                // Đóng chuỗi kết nối
+                oledbConn.Close();
+            }
+            return data;
+        }
+        private void CopyDataByBulk(DataTable dt)
+        {
+            //lay ket noi voi database luu trong file webconfig
+            SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["DB Context"].ConnectionString);
+            SqlBulkCopy bulkcopy = new SqlBulkCopy(con);
+            bulkcopy.DestinationTableName = "KhachHangs";
+            bulkcopy.ColumnMappings.Add(0, "TenKhachHang");
+            bulkcopy.ColumnMappings.Add(1, "MaKhachHang");
+            con.Open();
+            bulkcopy.WriteToServer(dt);
+            con.Close();
         }
 
         protected override void Dispose(bool disposing)
